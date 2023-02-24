52df5af48eac3fb41104c2821aac8be5a8280f85
Korper78 <korper78@gmail.com>
2023-02-24 11:00:02 +0300

    Day2, +redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Provider } from "react-redux"
 import { Catalog } from "./components/Catalog/Catalog.jsx"
 import { Footer } from "./components/Footer/Footer.jsx"
 import { Header } from "./components/Header/Header.jsx"
 import { Navigation } from "./components/Navigation/Navigation.jsx"
+import { store } from "./store/index.js"
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import './App.css'
@@ -10,13 +12,13 @@ export const App = () => {
   // const [count, setCount] = useState(0)
 
   return (
-  <>
+  <Provider store={store}>
     <Header />
     <main>
       <Navigation />
       <Catalog />
     </main>
     <Footer />
-  </>
+  </Provider>
   )
 }
